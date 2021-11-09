86c22e664f2f8cc0a63cb432f45e4caa37e6a05d
Garima <garimac26@gmail.com>
2021-11-09 09:54:10 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+print('Are you ready to add the numbers?')
+Firstnumber= float(input('Enter first number'))
+Secondnumber=float(input('Enter second number'))
+Sum=Firstnumber + Secondnumber
+print(float(Sum))
+print('Congrats! You are doing great')
